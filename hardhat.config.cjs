bc7afa5ca97ca22ab88fa6089d6a0bad851df9da
rickalon <main.rickalon@gmail.com>
2026-03-04 15:57:40 -0400

    chore: reduce count in the hardhat accounts configuration

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -9,7 +9,7 @@ module.exports = {
         mnemonic: 'anger burst story spy face pattern whale quit delay fiction ball solve',
         path: "m/44'/60'/0'/0",
         initialIndex: 0,
-        count: 2,
+        count: 1,
         accountsBalance: '1000000000000000000000'
       }
     }
